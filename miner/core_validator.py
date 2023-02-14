b3b748c127da89244055410d6cd9ed43c3f11449
Eugene <etesting007@gmail.com>
2023-02-14 13:23:24 -0800

    setup.py and updates

--- miner/core_validator.py
+++
@@ -1,5 +0,0 @@
-
-from validator.neurons.core_neuron import neuron
-
-if __name__ == "__main__":
-    template = neuron().run()
---
+++ miner/nakamoto_validator.py
@@ -0,0 +1,4 @@
+from validators import nakamoto
+
+if __name__ == "__main__":
+    template = nakamoto.neuron().run()
