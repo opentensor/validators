eedbb880fff2b0e9118364b03d9cd7a2877a4392
Eugene <etesting007@gmail.com>
2023-06-09 08:49:06 -0700

    version 1.0.3

--- openvalidators/__init__.py
+++ openvalidators/__init__.py
@@ -27,5 +27,6 @@ from . import run
 from . import utils
 from . import weights
 
-__version__ = "1.0.2"
-__spec_version__ = 1003
+__version__ = "1.0.3"
+version_split = __version__.split(".")
+__spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
