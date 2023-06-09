8f08fd6567cf46e83469e991776e2b03b97c34f2
Steffen Cruz <steffenjcruz@gmail.com>
2023-06-09 11:29:45 -0600

    Change exact equality condition to allclose, because of floating point diffs

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -21,20 +21,19 @@ import sys
 from openvalidators.neuron import neuron as Neuron
 from openvalidators.forward import forward
 
-CLI_ARGS = (
-    "validators/openvalidators/neuron.py --mock --wallet._mock --wallet.reregister False --wandb.off --logging.trace".split(
-        " "
-    )
-)
-argv = sys.argv.copy()
+CLI_ARGS_STR = "validators/openvalidators/neuron.py --mock --wallet._mock --wandb.off --neuron.followup_sample_size 10 --neuron.answer_sample_size 10"
+
+SYS_ARGV = sys.argv.copy()
 
 
 def test_uid_weights_unchanged_unless_queried(n_steps=10, n_concurrent=1):
     """Test that the weights of unqueried uids do not over the course of a forward pass."""
 
-    sys.argv = CLI_ARGS
+    sys.argv = CLI_ARGS_STR.split(" ")
     neuron = Neuron()
+
     for _ in range(n_steps):
+
         prev_scores = copy.deepcopy(neuron.moving_averaged_scores)
 
         # run concurrent forward passes
@@ -43,21 +42,16 @@ def test_uid_weights_unchanged_unless_queried(n_steps=10, n_concurrent=1):
             return await asyncio.gather(*coroutines)
 
         events = neuron.loop.run_until_complete(run_forward())
-
-        # get current scores
-        next_scores = neuron.moving_averaged_scores
-
-        print(f"Events has length {len(events)}")
         # moving_averaged_scores updates are not thread safe, so I don't think we can run concurrent forwards
         for event in events:
 
-            queried_uids = list(set(event["followup_uids"] + event["answer_uids"]))
+            # get current scores
+            next_scores = copy.deepcopy(neuron.moving_averaged_scores)
+
+            queried_uids = sorted(set(event["followup_uids"] + event["answer_uids"]))
             ignored_uids = [uid for uid in torch.arange(neuron.metagraph.n.item()) if uid not in queried_uids]
 
-            print(f"Queried uids: {queried_uids}")
-            print(f"Previous weights of queried uids: {next_scores[queried_uids]}")
-            print(f"New weights of queried uids: {prev_scores[queried_uids]}")
+            # ther is a floating point difference (~1e-10) between the scores, so we can't use exact equality
+            assert next_scores[ignored_uids].allclose(prev_scores[ignored_uids]), "Unqueried uids should not change"
 
-            assert all(next_scores[ignored_uids] == prev_scores[ignored_uids]), "Unqueried uids should not change"
-
-    sys.argv = argv.copy()
+    sys.argv = SYS_ARGV.copy()
