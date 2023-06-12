306a9925e718f99e536475ccd09de10561b3c04b
Ala Shaabana <shaabana@gmail.com>
2023-06-12 17:17:49 -0400

    Update __init__.py

--- openvalidators/__init__.py
+++ openvalidators/__init__.py
@@ -28,5 +28,6 @@ from . import utils
 from . import weights
 
 __version__ = "1.0.4"
+
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
