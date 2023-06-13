8fec8a9692a7e4ac1241eba4553b5e587a54dd75
p-ferreira <38992619+p-ferreira@users.noreply.github.com>
2023-06-13 10:43:02 -0400

    complements schema with field information

--- scripts/datasets_collector/schemas.py
+++ scripts/datasets_collector/schemas.py
@@ -1,25 +1,25 @@
 import pyarrow as pa
 
 DATASET_SCHEMA = pa.schema([
-    ('_runtime', pa.float64()),
-    ('_step', pa.int64()),
-    ('_timestamp', pa.float64()),
-    ('answer_completions', pa.list_(pa.string())),
-    ('answer_prompt', pa.string()),
-    ('answer_rewards', pa.list_(pa.float64())),
-    ('answer_times', pa.list_(pa.float64())),
-    ('answer_uids', pa.list_(pa.int32())),
-    ('base_prompt', pa.string()),
-    ('best_answer', pa.string()),
-    ('best_followup', pa.string()),
-    ('block', pa.float64()),
-    ('followup_completions', pa.list_(pa.string())),
-    ('followup_rewards', pa.list_(pa.float64())),
-    ('followup_times', pa.list_(pa.float64())),
-    ('followup_uids', pa.list_(pa.int64())),
-    ('gating_loss', pa.float64()),
-    ('gating_scorings', pa.list_(pa.float64())),
-    ('moving_averaged_scores', pa.list_(pa.float64())),
-    ('set_weights', pa.list_(pa.list_(pa.float64()))),
-    ('step_length', pa.float64())
+    ('_runtime', pa.float64()), # Runtime of the event
+    ('_step', pa.int64()), # Step of the event
+    ('_timestamp', pa.float64()), # Timestamp of the event
+    ('answer_completions', pa.list_(pa.string())), # Completions of the answer_prompt
+    ('answer_prompt', pa.string()), # Prompt used to generate the answer
+    ('answer_rewards', pa.list_(pa.float64())), # Rewards of the answer responses
+    ('answer_times', pa.list_(pa.float64())), # Ellapsed time of answer responses
+    ('answer_uids', pa.list_(pa.int32())), # UIDs of nodes that answered the answer_prompt
+    ('base_prompt', pa.string()), # Bootstrap prompt
+    ('best_answer', pa.string()), # Best answer response
+    ('best_followup', pa.string()), # Best followup response
+    ('block', pa.float64()), # Subtensor current block
+    ('followup_completions', pa.list_(pa.string())), # Completions of the base_prompt
+    ('followup_rewards', pa.list_(pa.float64())), # Rewards of the followup responses
+    ('followup_times', pa.list_(pa.float64())), # Ellapsed time of followup responses
+    ('followup_uids', pa.list_(pa.int64())), # UIDs of nodes that answered the base_prompt
+    ('gating_loss', pa.float64()), # Gating model loss
+    ('gating_scorings', pa.list_(pa.float64())), # Gating model scores
+    ('moving_averaged_scores', pa.list_(pa.float64())), # Moving averaged scores at the time of the event
+    ('set_weights', pa.list_(pa.list_(pa.float64()))),  # Processed weights of nodes by uid
+    ('step_length', pa.float64()) # Time difference from beginning of forward call to event logging
 ])
