91fd10791a5b01c2b877b12b01fe09d6979055fe
unconst <jake@bittensor.com>
2023-06-19 11:26:34 -0500

    .

--- openvalidators/neuron.py
+++ openvalidators/neuron.py
@@ -136,7 +136,7 @@ class neuron:
                 ReciprocateRewardModel( device = self.device ) if not self.config.neuron.reciprocate_off else MockRewardModel('mock-reciprocate'),
                 BertRelevanceRewardModel( device = self.device ) if not self.config.neuron.relevance_off else MockRewardModel('mock-relevance'),
                 DahoasRewardModel( path = self.config.neuron.full_path, device = self.device ) if not self.config.neuron.dahoas_off else MockRewardModel('mock-dahoas'),
-                DiversityRewardModel() if not self.config.neuron.diversity_off else MockRewardModel('mock-diversity'),
+                DiversityRewardModel( device = self.device ) if not self.config.neuron.diversity_off else MockRewardModel('mock-diversity'),
             ]
             bt.logging.debug(str(self.reward_functions))
 
