3bce98e15a8927d63e24430f31c367503e98cd26
Eugene <etesting007@gmail.com>
2023-06-21 10:03:47 -0700

    prompt

--- openvalidators/forward.py
+++ openvalidators/forward.py
@@ -88,11 +88,12 @@ async def run_step( self, prompt: str, k: int, timeout: float, name: str, exclud
     completions: List[str] = [comp.completion for comp in responses ]
     best:str = completions[ rewards.argmax( dim = 0 )].strip()
     
+    print('prompt', prompt)
     for ind, comp in enumerate(completions):
         print('********')
         print('reward:',comp, rewards[ind])
         print('********')
-        
+
     # Compute forward pass rewards, assumes followup_uids and answer_uids are mutually exclusive.
     # shape: [ metagraph.n ]
     scattered_rewards: torch.FloatTensor = self.moving_averaged_scores.scatter( 0, uids, rewards ).to(self.device) 
