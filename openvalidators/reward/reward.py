70438d7da15b0db41d23107f57eb15fbbad2834f
unconst <jake@bittensor.com>
2023-06-21 09:46:59 -0500

    add normalization function into rewards

--- openvalidators/reward/reward.py
+++ openvalidators/reward/reward.py
@@ -31,18 +31,58 @@ class BaseRewardModel:
     @abstractmethod
     def reward( self, prompt: str, completion: str, name: str ) -> float: ...
 
+    def normalize_rewards( self, rewards: torch.FloatTensor ) -> torch.FloatTensor:
+        """
+            This method normalizes the given rewards by updating the moving mean and variance statistics. The rewards are first standardized, and then scaled to the 0-1 range using a cumulative distribution function (CDF) to ensure they're in a comparable range across different environments.
+
+            Args:
+            rewards (torch.FloatTensor): The reward values to be normalized.
+
+            Returns:
+            torch.FloatTensor: The normalized reward values.
+
+            Note:
+            - This function uses Welford's online algorithm to update the mean and variance.
+            - It standardizes the reward values using the updated mean and variance.
+            - It then scales the standardized values to the 0-1 range using the error function (erf) as a CDF.
+        """
+        # Calculate the mean, count, and standard deviation of the new rewards.
+        new_mean = rewards.mean()
+        new_count = rewards.count()
+        new_var = rewards.std()
+
+        # Compute the weights for the new and old rewards.
+        new_weight = new_count / (self.old_count + new_count)
+        old_weight = self.old_count / (self.old_count + new_count)
+
+        # Update the old mean with the new mean and weights.
+        self.old_mean = new_weight * new_count + old_weight * self.old_mean
+        # Update the old variance with the new variance and weights, and adjusting for the difference in means.
+        self.old_var = (new_weight * new_var) + (old_weight * self.old_var) + (new_weight * old_weight) * (new_mean - self.old_mean) ** 2
+        # Update the old count with the new count, but don't exceed the limit.
+        self.old_count = min(self.count_limit, self.old_count + new_count)
+
+        # Standardize the rewards using the updated mean and variance.
+        rewards = (rewards - self.old_mean) / torch.sqrt(self.old_var)
+        # Scale the standardized rewards to the range [0, 1] using the error function as a cumulative distribution function (CDF).
+        rewards = 0.5 * (1 + torch.erf(rewards / torch.sqrt(torch.tensor([2.0]))))
+
+        return rewards
+
     def apply( self, prompt: str, responses: List[ bt.DendriteCall ], name: str) -> torch.FloatTensor:
         """ Applies the reward model across each call. Unsuccessful responses are zeroed.
         """
-
         # Get indices of correctly responding calls.
         successful_completions_indices: List[int] = [ idx for idx, resp in enumerate(responses) if resp.is_success ]
 
         # Get all completions from responding calls.
-        successful_completions: List[str] = [ responses[idx].completion.strip() for idx in successful_completions_indices ]
+        successful_completions: List[str] = [ responses[idx].completion.strip() for idx in successful_completions_indices]
 
         # Reward each completion.
         successful_rewards = torch.tensor( [ self.reward( prompt, completion, name ) for completion in successful_completions ], dtype = torch.float32 )
+
+        # Softmax rewards across samples.
+        successful_rewards = self.normalize_rewards( successful_rewards )
 
         # Init zero rewards for all calls.
         filled_rewards = torch.zeros( len( responses ), dtype=torch.float32)
