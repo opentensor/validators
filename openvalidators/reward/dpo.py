e8a65f2c1e169776b357ff7144c386ee2ef7148b
Eugene <etesting007@gmail.com>
2023-08-25 11:41:33 -0700

    dpo 1.2

--- openvalidators/reward/dpo.py
+++ openvalidators/reward/dpo.py
@@ -33,7 +33,7 @@ class DirectPreferenceRewardModel(BaseRewardModel):
     def __init__(self, device: str):
         super().__init__()
         self.device = device
-        self.penalty = 1.5
+        self.penalty = 1.2
         self.tokenizer = AutoTokenizer.from_pretrained(DirectPreferenceRewardModel.reward_model_name)
         self.model = AutoModelForCausalLM.from_pretrained(DirectPreferenceRewardModel.reward_model_name,
                                                           trust_remote_code=True,
